842341474c4059f257aec1bf71d658296b578ca5
Alonso Lagos <a.lagos10@ufromail.cl>
2025-04-04 00:11:11 -0300

    -- creado main.java --

---
+++ src/main/java/org/example/Carta.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Carta {
+     // creada la clase Carta, en este caso defini sus valores principales que tendra, el valor en si de la carta y su String.
+
+    private int valor;
+    private String pinta;
+
+
+    private static int getValor(){
+        return 0;
+    }
+
+    private static String getPinta(){
+        return "x";
+    }
+
+    private static String toString1(){
+        return null;
+    }
+
+    private static boolean esIgual(){
+        return true;
+    }
+
+
+}
