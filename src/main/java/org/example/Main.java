c4516f92158677c757848b546509e6c6ef7ad7f8
Alonso Lagos <a.lagos10@ufromail.cl>
2025-04-04 14:49:34 -0300

    main actualizado por ahora solo para testear las distintas clases y funciones de mi programa uwu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,18 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Mazo mazo = new Mazo();
+        mazo.crearMazo();
+        mazo.barajar();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Jugador jugador1 = new Jugador("Alonso");
+
+        for (int i = 0; i < 12; i++){
+            jugador1.robarCarta(mazo);
         }
+
+        jugador1.mostrarMano();
+
+
     }
 }
